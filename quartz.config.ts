e0268902dc8da3448fe0c75abc5f670ca41a729d
Md. Ashraful Islam <mdashrafulislam.inbox@gmail.com>
2024-12-17 22:35:52 +0600

    Quartz sync: Dec 17, 2024, 10:35 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴_backend",
+    pageTitle: "🍉_backend",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
